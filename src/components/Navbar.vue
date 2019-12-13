a9cb5e91d678cb355147e01ad05dc3fbbd426649
sarthakjha <sarthak.jha@gdn-commerce.com>
2019-12-13 18:39:01 +0530

    vuetify plugin installed

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-toolbar>
+            <v-toolbar-title>CodieCon</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items>
+                <v-btn text>Newsfeeds</v-btn>
+                <v-btn text>Forms</v-btn>
+                <v-btn text>Myfeeds</v-btn>
+                <v-btn>Login</v-btn>
+                <v-btn>Signup</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
