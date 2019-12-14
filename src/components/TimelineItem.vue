1af83e200689e4fb5180ccbbda22c07df8ad1c4d
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-12-14 11:34:07 +0530

    Timeline added to homepage

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-timeline-item large>
+        <template v-slot:icon>
+            <v-avatar>
+            <img :src="timelinedata.img">
+            </v-avatar>
+        </template>
+        <template v-if="alignment" v-slot:opposite>
+            <v-card class="elevation-2" @click="selectedOption(timelinedata.heading)">
+                <v-card-title class="headline">{{timelinedata.heading}}</v-card-title>
+                <v-card-text style="text-align: left;">{{timelinedata.desc}}</v-card-text>
+            </v-card>
+        </template>
+        <template v-if="!alignment">
+            <v-card class="elevation-2" @click="selectedOption(timelinedata.heading)">
+                <v-card-title class="headline">{{timelinedata.heading}}</v-card-title>
+                <v-card-text>{{timelinedata.desc}}</v-card-text>
+            </v-card>
+        </template>
+
+    </v-timeline-item>
+</template>
+
+<script>
+export default {
+    name: 'timelineitem',
+    props: {
+        timelinedata: {
+            type: Object,
+            required: true,
+            default: () => []
+        }
+    },
+    computed: {
+        alignment () {
+            if (this.timelinedata.index % 2 === 0 ) {
+                return true
+            }
+            return false
+        }
+    },
+    methods: {
+        selectedOption (id) {
+            // console.log('id', id)
+            this.$emit('categorychoosen', id)
+        }
+    }
+}
+</script>
+
+<style>
+.leftheading {
+    text-align: left;
+}
+</style>
--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -10,65 +10,129 @@
                     <v-btn v-for="tag in tags" :key="tag" class="tag_button">{{tag}}</v-btn>
                 </div>
             </section>
+            <section>
+                <div class="heading">
+                    <h1>Our Solutions</h1>
+                </div>
+                <v-timeline>
+                    <div v-for="data in timelinedata" :key="data.index">
+                        <timelineitem :timelinedata="data" @categorychoosen="selectCategory">
+                        </timelineitem>
+                    </div>
+                </v-timeline>
+            </section>
         </div>
     </main>
 </template>
 
 <script>
-    export default {
-        name: "LandingPage",
-        data () {
-            return {
-                tags: [
-                    'all',
-                    'tall',
-                    'ball'
-                ]
-            }
+import timelineitem from '../components/TimelineItem'
+
+export default {
+    name: "LandingPage",
+    components: {
+        timelineitem
+    },
+    data () {
+        return {
+            tags: [
+                'all',
+                'tall',
+                'ball'
+            ],
+            timelinedata: [{
+                'index': 0,
+                'heading': 'Buy',
+                'desc' : 'You can buy things within the organisation from the people around you',
+                'img' : '../assets/categoryicon/buy.svg'
+            },{
+                'index': 1,
+                'heading': 'Sell',
+                'desc' : 'You can sell things within the organisation from the people around you',
+                'img' : '../assets/categoryicon/sell.svg'
+            },{
+                'index': 2,
+                'heading': 'Rent',
+                'desc' : 'You can rent things within the organisation from the people around you',
+                'img' : '../assets/categoryicon/rent.svg'
+            },{
+                'index': 3,
+                'heading': 'Borrow',
+                'desc' : 'You can borrow things within the organisation from the people around you',
+                'img' : '../assets/categoryicon/loan.svg'
+            },{
+                'index': 4,
+                'heading': 'Activity',
+                'desc' : 'You can create activities like sports etc within the organisation from the people around you',
+                'img' : '../assets/categoryicon/social.svg'
+            },{
+                'index': 5,
+                'heading': 'Flat Sharing',
+                'desc' : 'You can find flatmates within the organisation from the people around you',
+                'img' : '../assets/categoryicon/flat.svg'
+            },{
+                'index': 6,
+                'heading': 'And More to Come...',
+                'desc' : 'We promise to keep on addding more services on discussion board...',
+                'img' : '../assets/categoryicon/flat.svg'
+            }]
+        }
+    },
+    methods: {
+        selectCategory (name) {
+
         }
     }
+}
 </script>
 
 <style scoped>
-    .parallax {
-        /* The image used */
-        background-image: url("../assets/low_poly_smoke_purple-2560x1440.jpg");
-        /* Set a specific height */
-        min-height: 1000px;
+.heading {
+    align-self: center;
+    background: #ffffff;
+    border-radius: 10px;
+    margin: 100px 500px 0px 500px;
+    text-align: center;
+}
+.parallax {
+    /* The image used */
+    background-image: url("../assets/low_poly_smoke_purple-2560x1440.jpg");
+    /* Set a specific height */
+    min-height: 1000px;
 
-        /* Create the parallax scrolling effect */
-        background-attachment: fixed;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    .welcome {
-        min-height: 400px;
-    }
-    .search {
-        position: relative;
-        color: #777;
-        opacity: 0.6;
-        background-color: #ffffff;
-        filter: alpha(opacity=60);
-        border-radius: 30px; 
-        text-align:center;
-        min-height: 50px;
-        min-width: 500px;
-        margin: 14vw 55vh;
-        outline: none;
-    }
-    .paragraph {
-        color: #777;
-        opacity: 0.6;
-        background-color: #ffffff;
-        filter: alpha(opacity=60);
-        text-align:center;
-        min-height: 300px;
-        padding:50px 80px;
-        text-align: center;
-        font-size: 30px;
-        font-weight: bold;
-    }
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.welcome {
+    min-height: 400px;
+}
+.search {
+    position: relative;
+    color: #777;
+    opacity: 0.6;
+    background-color: #ffffff;
+    filter: alpha(opacity=60);
+    border-radius: 30px; 
+    text-align:center;
+    min-height: 50px;
+    min-width: 500px;
+    margin: 14vw 55vh;
+    outline: none;
+}
+.paragraph {
+    color: #777;
+    opacity: 0.6;
+    background-color: #ffffff;
+    filter: alpha(opacity=60);
+    text-align:center;
+    min-height: 300px;
+    padding:50px 80px;
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+}
     
 </style>
