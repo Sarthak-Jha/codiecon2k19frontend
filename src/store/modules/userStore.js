0f37b61afd50384417d82d163def02ca53168f3f
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-12-15 10:38:17 +0530

    Store structure and config structure

---
+++ src/store/modules/userStore.js
@@ -0,0 +1,20 @@
+export const state = {
+}
+
+export const mutations = {
+}
+
+export const actions = {
+
+}
+
+export const getters = {
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
+}
