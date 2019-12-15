9aa73c634107e133fa84c8113652e10c37799178
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-12-15 17:44:54 +0530

    Stores

--- src/store/modules/userStore.js
+++ src/store/modules/userStore.js
@@ -1,14 +1,53 @@
+import MainApi from '../../api/api'
+
 export const state = {
+    UserDetails: {
+        status : false
+    }
 }
 
 export const mutations = {
+    setUserDetails(state, payload) {
+		//debugger
+		state.userDetails = payload;
+	}
 }
 
 export const actions = {
-
+    validateLogin ({commit}, {data, success, fail}) {
+        MainApi.validateLogin( (res) => {
+            success && success(res)
+        }, data, (res) => {
+            fail && fail()
+        })
+    },
+    validateRegister ({commit}, {data, success, fail}) {
+        MainApi.validateRegister( (res) => {
+            success && success(res)
+        }, data, (res) => {
+            fail && fail()
+        })
+    },
+    verifyRegister ({commit}, {data, success, fail}) {
+        MainApi.verifyRegister( (res) => {
+            success && success(res)
+        }, data, (res) => {
+            fail && fail()
+        })
+    },
+    doLogout ({commit}, {data, success,fail}) {
+        MainApi.doLogout( (res) => {
+            success && success(res)
+        }, data, (res) => {
+            fail && fail()
+        })
+    }
 }
 
 export const getters = {
+    isLoggedIn(state) {
+		return state.userDetails;
+	}
 }
 
 export default {
