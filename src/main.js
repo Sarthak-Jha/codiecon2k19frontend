9d36964a21ce4ce1b66e28683b7e5e7d0a0fb0af
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-12-18 18:13:03 +0530

    Vue Cookies

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ import VueSession from 'vue-session'
 import VueSimpleAlert from 'vue-simple-alert'
 import VueCookie from 'vue-cookie'
 
+Vue.use(VueSimpleAlert)
+Vue.use(VueSession)
+Vue.use(VueCookie)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
-  VueCookie,
-  VueSession,
-  VueSimpleAlert,
   render: h => h(App)
 }).$mount('#app')
