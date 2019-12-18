5f71a206f422d23b931fadf3d416a7160ab3d222
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-12-18 18:12:44 +0530

    Vue Cookies Added

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import VueSession from 'vue-session'
-import VueSimpleAlert from "vue-simple-alert";
-
-Vue.use(VueSimpleAlert);
-Vue.use(VueSession)
+import VueSimpleAlert from 'vue-simple-alert'
+import VueCookie from 'vue-cookie'
 
 Vue.config.productionTip = false
 
@@ -15,5 +13,8 @@ new Vue({
   router,
   store,
   vuetify,
+  VueCookie,
+  VueSession,
+  VueSimpleAlert,
   render: h => h(App)
 }).$mount('#app')
